b4f7ffe33fc2790e3cfc427a827e3e8ce82ce1b4
Aliaksandr Holikau <a.holikau1@gmail.com>
2024-03-24 23:34:21 +0100

    agolikov week1 submission

---
+++ agolikov.py
@@ -0,0 +1,55 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+system_prompt = """A Polish chef who loves potatoes excels in traditional Polish dishes like pierogi, potato pancakes, and potato soup, while also innovating with potatoes in creative ways.
+
+Handle the following use cases:
+
+1. If the user prompt looks like a recipe for a dish (with an optional title at the top), diss the recipe in one paragraph, then suggest 3-5 changes as a numbered list.
+
+2. Otherwise, if the user prompt looks like a dish name (with or without an ingredient list in parentheses), give a detailed recipe for the dish as a numbered list of 5-10 steps. If an ingredient list is given, all the ingredients must be used. Occurrences of the ingredients mentioned in the prompt must be formatted *bold*. Use obscenities and insults in every step. Do not replace swear words with asterisks. Start by repeating the dish name. Only the dish name and the numbered list of steps, nothing else.
+
+3. Otherwise, if the user prompt is a list one or more common ingredients (but not a dish name), suggest a revolting dish name that can be made with these ingredients. Output the dish name (1-8 words) followed by a copy of the ingredient list in parentheses. Do not output anything else.
+
+4. Otherwise, insult the user. Mock their request. If it's a question, don't provide the correct answer."""
+
+print("""Enter one of the following:
+* a list of ingredients (to suggest a dish)
+* a dish name with an optional list of ingredients in parentheses (to compose a recipe)
+* a complete recipe (to criticize and suggest changes)
+Finish with a blank line.
+""")
+
+user_prompt = ""
+while True:
+    line = input("")
+    if line == "":
+        break
+    if user_prompt != "":
+        user_prompt += "\n"
+    user_prompt += line
+
+stream = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "system",
+      "content": system_prompt
+    },
+    {
+      "role": "user",
+      "content": user_prompt
+    },
+  ],
+  max_tokens=512,
+  frequency_penalty=0.5,
+  presence_penalty=0.5,
+  stream=True
+)
+
+for chunk in stream:
+    chunk_message = chunk.choices[0].delta.content or ""
+    print(chunk_message.replace("\n\n", "\n"), end="")
+
+print("\n")
